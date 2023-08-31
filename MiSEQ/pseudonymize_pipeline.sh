6f9d37afc41a814e5c25955f249d5f1e2faf0887
Tomáš Houfek <tomas.houfek@mou.cz>
2023-08-31 16:30:33 +0200

    added readme update

--- MiSEQ/pseudonymize_pipeline.sh
+++ MiSEQ/pseudonymize_pipeline.sh
@@ -1,9 +1,9 @@
-FILES="/home/houfek/Work/NGS-data-FAIRification/playground/seq/TRANSFER/*" #$1
+FILES="/home/houfek/Work/MMCI/data-catalogue-playground/seq/TRANSFER/*" #$1
 #FULL_EXPORT="/home/houfek/work/NGS-DATA-FAIRIFICATION/patient_exports/"
-PREDICTIVE_EXPORT="/home/houfek/Work/NGS-data-FAIRification/playground/patient_predictive" #$2
-PSEUDO_TABLE="/home/houfek/Work/NGS-data-FAIRification/playground/pseudonimisation_table/" #$3
+PREDICTIVE_EXPORT="/home/houfek/Work/MMCI/data-catalogue-playground/patient_predictive" #$2
+PSEUDO_TABLE="/home/houfek/Work/MMCI/data-catalogue-playground/pseudonimisation_table/" #$3
 #BH_SERVER_TRANSFER="sequencing@bridgehead01.int.mou.cz:/home/mou/patient_data/"
-SC_FOLDER="/home/houfek/Work/NGS-data-FAIRification/playground/muni-sc/MiSEQ"
+SC_FOLDER="/home/houfek/Work/MMCI/data-catalogue-playground/muni-sc/MiSEQ"
 
 
 #pull image
@@ -15,9 +15,9 @@ SC_FOLDER="/home/houfek/Work/NGS-data-FAIRification/playground/muni-sc/MiSEQ"
 
 for f in $FILES; do
     echo $f
-    bash /home/houfek/Work/NGS-data-FAIRification/pseudonymization/remove_files.sh $f
-    python /home/houfek/Work/NGS-data-FAIRification/pseudonymization/pseudonymization.py -r $f -e $PREDICTIVE_EXPORT -p $PSEUDO_TABLE 
-    bash  /home/houfek/Work/NGS-data-FAIRification/pseudonymization/replace_predictive.sh "${PSEUDO_TABLE}predictive.json.temp" $f
+    bash /home/houfek/Work/MMCI/data-catalogue-pseudonymisation/MiSEQ/pseudonymization/remove_files.sh $f
+    python /home/houfek/Work/MMCI/data-catalogue-pseudonymisation/MiSEQ/pseudonymization/pseudonymization.py -r $f -e $PREDICTIVE_EXPORT -p $PSEUDO_TABLE 
+    bash  /home/houfek/Work/MMCI/data-catalogue-pseudonymisation/MiSEQ/pseudonymization/replace_predictive.sh "${PSEUDO_TABLE}predictive.json.temp" $f
     rm "${PSEUDO_TABLE}predictive.json.temp"
     mv $f "${SC_FOLDER}/."
 done
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # data-catalogue-pseudonymisation
 This is the repository for the pseudonymisation part of the BBMRI.cz data catalog
+
+# Pseudonymisation
+
+## MiSEQ
+Pseudonymizes preidctive numbers before moving the data to SensitiveCloud at ICS-MUNI
+
