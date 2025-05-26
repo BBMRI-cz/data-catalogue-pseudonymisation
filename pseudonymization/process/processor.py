778eb68d4a55f0f7731462432a28135e15a58a40
Matúš Formánek <formanekmato@gmail.com>
2025-05-26 10:18:37 +0200

    feat: pseudonymized NextSeq are moved to the same folder as all other runs

--- pseudonymization/process/processor.py
+++ pseudonymization/process/processor.py
@@ -14,11 +14,9 @@ from pseudonymization.pseudonymizers.nextseq_pseudonymizer import NextSeqPseudon
 class Processor:
 
     def __init__(self, sequencing_run: str, destination_folder: str,
-                 destination_next_seq_folder: str, pseudo_tables_folder: str,
-                 sequencing_libraries: str, sequencing_libraries_sc: str):
+                 pseudo_tables_folder: str, sequencing_libraries: str, sequencing_libraries_sc: str):
         self.sequencing_file_path: str = sequencing_run
         self.destination_folder: str = destination_folder
-        self.destination_next_seq_folder: str = destination_next_seq_folder
         self.pseudonymization_tables_folder: str = pseudo_tables_folder
         self.sequencing_libraries_folder = sequencing_libraries
         self.sequencing_libraries_folder_sc = sequencing_libraries_sc
@@ -28,10 +26,7 @@ class Processor:
             full_run_path = os.path.join(self.sequencing_file_path, run)
             pseudonymizer = self._initialize_based_on_record_type(full_run_path)
             pseudonymizer.pseudonymize()
-            if isinstance(pseudonymizer, NextSeqPseudonymizer):
-                self._mv_psedonymizer_run_to_sc_nextseq(run)
-            else:
-                self._mv_pseudonymizer_run_to_sc(run)
+            self._mv_pseudonymizer_run_to_sc(run)
 
     def copy_libraries(self):
         self.touch_all_files(self.sequencing_libraries_folder)
@@ -49,10 +44,6 @@ class Processor:
     def _mv_pseudonymizer_run_to_sc(self, run_name):
         shutil.move(os.path.join(self.sequencing_file_path, run_name),
                     os.path.join(self.destination_folder, run_name))
-
-    def _mv_psedonymizer_run_to_sc_nextseq(self, run_name):
-        shutil.move(os.path.join(self.sequencing_file_path, run_name),
-                    os.path.join(self.destination_next_seq_folder, run_name))
 
     def _initialize_based_on_record_type(self, full_run_path) -> (FileRemover, RunPseudonymizer):
         if "SoftwareVersionsFile.csv" in os.listdir(full_run_path) or "Alignment_1" in os.listdir(full_run_path):
