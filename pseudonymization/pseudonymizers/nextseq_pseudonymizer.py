40bbbf4337600c841d8df44ac5ed69ee66c5a138
Matúš Formánek <525111@mail.muni.cz>
2025-07-29 14:14:33 +0200

    fix: nextseq parsing SampleSheet

--- pseudonymization/pseudonymizers/nextseq_pseudonymizer.py
+++ pseudonymization/pseudonymizers/nextseq_pseudonymizer.py
@@ -96,14 +96,12 @@ class NextSeqPseudonymizer(RunPseudonymizer):
 
     def _get_all_predictive_numbers_pseudonymize_sample_sheet(self):
         sample_sheet_path = os.path.join(self.run_path, "SampleSheet.csv")
-        df = pd.read_csv(sample_sheet_path,
-                         delimiter=",",
-                         names=["[Header]", "Unnamed: 1", "Unnamed: 2", "Unnamed: 3", "Unnamed: 4",
-                                "Unnamed: 5", "Unnamed: 6", "Unnamed: 7", "Unnamed: 8"])
+        df = pd.read_csv(sample_sheet_path, header=None)
+        sample_list_header = df[0].tolist()
+        sample_list_last = df[8].tolist()
 
-        sample_list_header = df["[Header]"].to_list()
-        sample_list_last = df["Unnamed: 8"].to_list()
         sample_ids_start = sample_list_header.index("Sample_ID") + 1
+
 
         predictive_numbers = sample_list_header[sample_ids_start:]
         predictive_pseudonimizer = PseudonymizePredictive(self.predictive_pseudo_table)
@@ -112,13 +110,12 @@ class NextSeqPseudonymizer(RunPseudonymizer):
         new_column_header = sample_list_header[:sample_ids_start] + pseudo_numbers
         new_column_last = sample_list_last[:sample_ids_start] + pseudo_numbers
 
-        df.drop(["[Header]", "Unnamed: 8"], axis=1, inplace=True)
-        df.insert(loc=0, column="[Header]", value=new_column_header)
-        df["Unnamed: 8"] = new_column_last
-        df.columns = ["[Header]"] + ["" for _ in range(len(df.columns) - 1)]
+        df[0] = new_column_header
+        df[8] = new_column_last  
         df.fillna("", inplace=True)
         df.to_csv(sample_sheet_path, header=False, index=False)
 
         predictive_pseudo_tuples = [(predictive_numbers[i], pseudo_numbers[i]) for i in range(len(predictive_numbers))]
 
         return predictive_pseudo_tuples
+ 
