9320fc37035b1cc79f9ce427ee9f91822ad14805
Matúš Formánek <525111@mail.muni.cz>
2025-09-18 19:49:47 +0300

    feat: added env var for logging level

--- pseudonymization/logging_config/logging_config.py
+++ pseudonymization/logging_config/logging_config.py
@@ -4,16 +4,31 @@ import sys
 
 class LoggingConfig:
     _logger : logging.Logger | None = None
-    _run_id = None
+    _run_id : str | None = None
+    LOG_LEVELS = {
+        "DEBUG": logging.DEBUG,
+        "INFO": logging.INFO,
+        "WARNING": logging.WARNING,
+        "ERROR": logging.ERROR,
+        "CRITICAL": logging.CRITICAL
+    }
 
     @classmethod
-    def initialize(cls, run_id, log_dir):
+    def initialize(cls, run_id: str, log_dir: str) -> logging.Logger:
         cls._run_id = run_id
         os.makedirs(log_dir, exist_ok=True)
         log_filename = os.path.join(log_dir, f"{run_id}.log")
 
         logger = logging.getLogger(f"run.{run_id}")
-        logger.setLevel(logging.INFO)
+
+        log_level_env = os.getenv("LOG_LEVEL", "INFO").upper()
+        if log_level_env not in cls.LOG_LEVELS:
+            raise ValueError(f"Invalid LOG_LEVEL '{log_level_env}'. Must be one of {list(cls.LOG_LEVELS.keys())}")
+
+        log_level = cls.LOG_LEVELS[log_level_env]
+
+        logger.setLevel(log_level)
+
 
         if not logger.handlers:  
             # file logging
@@ -30,6 +45,9 @@ class LoggingConfig:
             logger.addHandler(stream_handler)
 
         cls._logger = logger
+
+        logger.info(f"Logger initialized with level: {log_level_env}")
+
         return logger
 
     @classmethod
